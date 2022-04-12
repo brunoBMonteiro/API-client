3fe3152df560f2990631518f3bfc4315426cdab7
Bruno <brunobilheri@hotmail.com>
2022-04-12 13:29:58 -0300

    Adding swagger concluded

--- src/main/java/com/example/firstapi/controller/ClienteController.java
+++ src/main/java/com/example/firstapi/controller/ClienteController.java
@@ -4,8 +4,6 @@ import com.example.firstapi.model.Cliente;
 import com.example.firstapi.requests.ClientePostRequestBody;
 import com.example.firstapi.requests.ClientePutRequestBody;
 import com.example.firstapi.service.ClienteService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +15,6 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/clientes")
-@Api(value = "API REST Clientes")
 @CrossOrigin(origins = "*")
 public class ClienteController {
 
@@ -25,7 +22,6 @@ public class ClienteController {
 
 
     @GetMapping
-    @ApiOperation(value = "Retorna lista de clientes")
     public ResponseEntity<List<Cliente>> listAll() {
         //return ResponseEntity.ok(clienteService.listAll());
 
@@ -44,27 +40,23 @@ public class ClienteController {
     }
 
    @GetMapping(path = "/{id}")
-   @ApiOperation(value = "Retorna um cliente pelo seu id")
    public ResponseEntity<Cliente> findById(@PathVariable long id){
        return ResponseEntity.ok(clienteService.findByIdOrThrowBadRequestException(id));
 
    }
 
    @PostMapping
-   @ApiOperation(value = "Salva um novo cliente")
    public ResponseEntity<Cliente> save(@RequestBody ClientePostRequestBody clientePostRequestBody){
         return new ResponseEntity<>(clienteService.save(clientePostRequestBody), HttpStatus.CREATED);
    }
 
     @DeleteMapping(path = "/{id}")
-    @ApiOperation(value = "Deleta cliente pelo seu id")
     public ResponseEntity<Void> deleteClientById(@PathVariable long id) {
         clienteService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping(value = "/{id}")
-    @ApiOperation(value = "Atualiza informações do cliente")
     public ResponseEntity<Void> replace(@RequestBody ClientePutRequestBody clientePutRequestBody) {
         clienteService.replace(clientePutRequestBody);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
--- src/main/java/config/SwaggerConfig.java
+++ src/main/java/config/SwaggerConfig.java
@@ -1,48 +1,26 @@
 package config;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket clientApi(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.firstapi"))
-                .paths(regex("/clientes.*"))
-                .build()
-                .apiInfo(metaInfo());
-    }
-
-
-    private ApiInfo metaInfo() {
-        ApiInfo apiInfo = new ApiInfo(
-                "Clientes API REST",
-                "API REST de cadastro de clientes.",
-                "1.0",
-                "Terms of Service",
-                new Contact("Bruno Monteiro", "https://github.com/brunoBMonteiro",
-                        "brunobilheri@hotmail.com"),
-                "Apache License Version 2.0",
-                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
-        );
-        return apiInfo;
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("API REST Client")
+                                .description("Api para cadastro de clientes contendo nome, cpf, idade, endereço")
+                                .version("1.0.0")
+                                .contact(new Contact().email("brunobilheri@hotmail.com").name("Bruno Monteiro"))
+                );
     }
 
 }
