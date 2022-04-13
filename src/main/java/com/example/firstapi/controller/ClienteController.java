a52d3d421393f388871ec75ffbbfee4ff81950a2
Bruno <brunobilheri@hotmail.com>
2022-04-13 00:34:52 -0300

    Add Open API swagger and spring docs

--- src/main/java/com/example/firstapi/controller/ClienteController.java
+++ src/main/java/com/example/firstapi/controller/ClienteController.java
@@ -4,6 +4,12 @@ import com.example.firstapi.model.Cliente;
 import com.example.firstapi.requests.ClientePostRequestBody;
 import com.example.firstapi.requests.ClientePutRequestBody;
 import com.example.firstapi.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +22,18 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 @RequestMapping("/clientes")
 @CrossOrigin(origins = "*")
+@Tag(name = "Cliente", description = "a API cliente com anotações de documentação")
 public class ClienteController {
 
     private final ClienteService clienteService;
 
-
+    @Operation(summary = "Obter uma lista de clientes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "encontrou lista", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "204", description = "Tem uma lista mas está vazia", content = @Content),
+            @ApiResponse(responseCode = "404", description = "lista de clientes não encontrada", content = @Content)
+    })
     @GetMapping
     public ResponseEntity<List<Cliente>> listAll() {
         //return ResponseEntity.ok(clienteService.listAll());
@@ -39,23 +52,48 @@ public class ClienteController {
 
     }
 
+
+    @Operation(summary = "Obter um cliente por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "encontrou o cliente", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado", content = @Content)
+    })
    @GetMapping(path = "/{id}")
    public ResponseEntity<Cliente> findById(@PathVariable long id){
        return ResponseEntity.ok(clienteService.findByIdOrThrowBadRequestException(id));
 
    }
 
+
+    @Operation(summary = "Salva um novo cliente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "salvou novo cliente", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+    })
    @PostMapping
    public ResponseEntity<Cliente> save(@RequestBody ClientePostRequestBody clientePostRequestBody){
         return new ResponseEntity<>(clienteService.save(clientePostRequestBody), HttpStatus.CREATED);
    }
 
+    @Operation(summary = "Deleta um cliente por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "cliente deletado com sucesso", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado", content = @Content)
+    })
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> deleteClientById(@PathVariable long id) {
         clienteService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "Atualiza informações de um cliente por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Informações do cliente atualizadas", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado", content = @Content)
+    })
     @PutMapping(value = "/{id}")
     public ResponseEntity<Void> replace(@RequestBody ClientePutRequestBody clientePutRequestBody) {
         clienteService.replace(clientePutRequestBody);
--- src/main/java/config/SwaggerConfig.java
+++ src/main/java/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ package config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,10 +18,18 @@ public class SwaggerConfig {
                 .info(
                         new Info()
                                 .title("API REST Client")
-                                .description("Api para cadastro de clientes contendo nome, cpf, idade, endereço")
+                                .description("Api de cadastro de clientes criado usando spring docs, " +
+                                        "contendo nome, cpf, idade, endereço")
+                                .termsOfService("https://swagger.io/terms/")
                                 .version("1.0.0")
-                                .contact(new Contact().email("brunobilheri@hotmail.com").name("Bruno Monteiro"))
+                                .license(new License().name("Apache 2.0"))
+                                .contact(new Contact()
+                                        .email("brunobilheri@hotmail.com")
+                                        .name("Bruno Monteiro"))
                 );
     }
+
+
+
 
 }
