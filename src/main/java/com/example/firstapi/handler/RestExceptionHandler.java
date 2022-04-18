84020922faa0394f24bf7fc2a4474ead4b211c89
brunoBMonteiro <brunobilheri@hotmail.com>
2022-04-17 22:07:22 -0300

    Overriding spring handler

--- src/main/java/com/example/firstapi/handler/RestExceptionHandler.java
+++ src/main/java/com/example/firstapi/handler/RestExceptionHandler.java
@@ -1,14 +1,20 @@
 package com.example.firstapi.handler;
 
+import com.example.firstapi.exceptions.ExceptionDetails;
 import com.example.firstapi.exceptions.NotFoundRequestException;
 import com.example.firstapi.exceptions.NotFoundRequestExceptionDetails;
 import com.example.firstapi.exceptions.ValidationExceptionDetails;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,11 +23,12 @@ import java.util.stream.Collectors;
 //permite que você use exatamente as mesmas técnicas de tratamento de exceção,
 // mas aplique-as em toda a aplicação, não apenas a um controlador individual
 @ControllerAdvice
-public class RestExceptionHandler {
+@Log4j2
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     // Se lançar uma exceção e ela for do tipo NotFoundException, tem que utilizar o método
     @ExceptionHandler(NotFoundRequestException.class)
-    public ResponseEntity<NotFoundRequestExceptionDetails> handlerBadRequestException(NotFoundRequestException bre){
+    public ResponseEntity<NotFoundRequestExceptionDetails> handleBadRequestException(NotFoundRequestException bre){
         return new ResponseEntity<>(
                 NotFoundRequestExceptionDetails.builder()
                         .timestamp(LocalDateTime.now())
@@ -32,9 +39,9 @@ public class RestExceptionHandler {
                         .build(), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(
-            MethodArgumentNotValidException exception){
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request){
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
         String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
         String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(
@@ -50,6 +57,20 @@ public class RestExceptionHandler {
                         .fields(fields)
                         .fieldsMessage(fieldsMessage)
                         .build(), HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request){
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status.value())
+                .title(ex.getCause().getMessage())
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+
+        return new ResponseEntity<>(exceptionDetails, headers, status);
     }
 
 }
